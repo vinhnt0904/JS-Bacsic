1539771412ea434b3e45e9806f509c898eeb7074
Thái Vĩnh <tvinhsvp@gmail.com>
2023-10-23 01:58:09 +0700

    function

---
+++ function/function.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+// input parameter
+
+sum = (a, b) => a + b;
+
+console.log(sum(4, 6));
--- loop/loop.js
+++
@@ -1,8 +0,0 @@
-console.log(`hello world`)
-
-
-let arr = ['Mu', 'MC', 'TOT', 'Liver']
-
-for (let i = 0; i < arr.length; i++) {
-    console.log(`i = ${arr[i]}`)
-}
